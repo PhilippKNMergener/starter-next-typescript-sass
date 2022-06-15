fef921442294064778c0c187083908ed7d4e3634
PhilippKNMergener <philippknmergener@gmail.com>
2022-06-14 19:56:35 -0700

    custom boilerplate

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="Container">
+      Hello, Boilerplate!
+    </div>
+  )
+}
+
+export default Home
